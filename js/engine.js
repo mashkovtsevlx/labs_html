714f6617dc9b2ca520cea744d2b6449083ef43a2
Alex Mashkovtsev <mashkovtsev.lx@gmail.com>
2017-09-18 12:01:49 +0300

    First Commit

---
+++ js/engine.js
@@ -0,0 +1,20 @@
+function s_button(num) {
+    "use strict";
+    var slider = document.getElementById('slider'), s1 = document.getElementById('s_1'), s2 = document.getElementById('s_2'), s3 = document.getElementById('s_3');
+    if (num === 1) {
+        s1.style.background = "#e3e3e3";
+        s2.style.background = "white";
+        s3.style.background = "white";
+        slider.innerHTML = '<a href="q50.html"><img src="img/q50.jpg" alt="Infiniti Q50" /></a>';
+    } else if (num === 2) {
+        s1.style.background = "#white";
+        s2.style.background = "#e3e3e3";
+        s3.style.background = "white";
+        slider.innerHTML = '<a href="qx60.html"><img src="img/qx60.jpg" alt="Infiniti Q50" /></a>';
+    } else if (num === 3) {
+        s1.style.background = "white";
+        s2.style.background = "white";
+        s3.style.background = "#e3e3e3";
+        slider.innerHTML = '<a href="qx70.html"><img src="img/qx70.jpg" alt="Infiniti Q50" /></a>';
+    }
+}
